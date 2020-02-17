84e5d4db84e868fdff61e3d749ea924825c4da5b
BobTabrizi <btabrizi@ucsd.edu>
2020-02-17 13:32:51 -0800

    Initial Commit

---
+++ login.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+    
+        <title>Gift Handler</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel ="stylesheet" type = "text/css"
+      href= "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    
+              
+              
+</head>
+<body>
+    
+    <div class="container">
+    <div class="login-box"></div>
+    <div class = "row">
+    <div class = "col-md-6 login-left">
+        <h2>Gift Organizer</h2>
+        <form action = "validation.php" method ="post">
+            <div class = "form-group">
+                <label>Username</label>
+                <input type = "text" name = "user" class = "form-control" required>
+                
+        </div>
+            
+            <div class = "form-group">
+                <label>Password</label>
+                <input type = "password" name = "password" class = "form-control" required>
+            </div>
+            <button type="submit" class="btn btn-primary"> Login</button>
+            <button type="submit" class="btn btn-primary">Sign Up</button>
+        </form>  
+        </div>
+        
+        <div class = "col-md-6 login-right">
+        <h2>Register</h2>
+        <form action = "registration.php" method ="post">
+            <div class = "form-group">
+                <label>Username</label>
+                <input type = "text" name = "user" class = "form-control" required>
+                
+        </div>
+            
+            <div class = "form-group">
+                <label>Password</label>
+                <input type = "password" name = "password" class = "form-control" required>
+            </div>
+            <button type="submit" class="btn btn-primary"> Login</button>
+        </form>  
+        </div>
+        
+        
+        
+        
+    </div>
+    </div>
+    
+    
+    
+    </body>
+</html>
