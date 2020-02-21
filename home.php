b32852bf82cdbeac1fd0ef5b4752fddf9d6067b2
BobTabrizi <btabrizi@ucsd.edu>
2020-02-21 14:16:14 -0800

    Homepage UI Design improvements

--- home.php
+++ home.php
@@ -15,7 +15,7 @@ if(!isset($_SESSION['username'])){
 <head>
     
     <title> HomePage</title>
-      <link rel="stylesheet" type="text/css" href="style.css">
+      <link rel="stylesheet" type="text/css" href="style2.css">
     <link rel ="stylesheet" type = "text/css"
       href= "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     
@@ -24,10 +24,17 @@ if(!isset($_SESSION['username'])){
     <body>
     <div class="container">
         
-   
     <a class ="float-right" href="logout.php"> LOG OUT</a>
-        
-    <h1>Welcome <?php echo $_SESSION['username']; ?></h1>
+       
+ 
              </div>
+ 
+           <h3>Welcome <?php echo $_SESSION['username']; ?>,</h3>
+        <div id ="buttons">
+      <button type="button home-button" id="button1" >Add</button>
+      <button type="button contact-button" id="button2">Delete</button>
+        </div>
+        <div class = "gift-box">
+        </div>
     </body>
 </html>
